b0df9d194e2fa0a07456b6f70a9f0237cf9f6029
danilomacb <danilo.bakun@gmail.com>
2020-05-29 08:25:14 -0300

    starting backend

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", routes);
+
+const port = 3001;
+const mongodb_url = "mongodb://localhost:27017/loginJNRM";
+
+mongoose.connect(
+  mongodb_url,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  (err) => {
+    if (err) return console.error("Error on mongoose connect: ", err);
+  }
+);
+
+app.listen(port, () => console.log("listening at http://localhost:" + port));
