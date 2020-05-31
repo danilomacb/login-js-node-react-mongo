64c5922de1c1c6eb03b92849e5adfb54a15b2d21
danilomacb <danilo.bakun@gmail.com>
2020-05-31 11:45:44 -0300

    creating PrivateRoute

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import { isAuthenticated } from "./auth";
 import Navigation from "./components/Navigation";
 import Login from "./components/Login";
 import Register from "./components/Register";
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        )
+      }
+    />
+  );
+}
 
 function Routes() {
   return (
@@ -12,6 +28,7 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/register" component={Register} />
+        <PrivateRoute path="/app" component={() => <h1>Você está logado</h1>} />
       </Switch>
     </BrowserRouter>
   );
